0a9b9bfb66e72e1e481f52fe9a55802d10e784dd
AnthonyWRodriguez <50894647+AnthonyWRodriguez@users.noreply.github.com>
2019-12-19 18:31:35 -0600

    Revert "Add files via upload"

--- simplePHPMVC-nw201903(vacio)/docs/scripts/05_create_table_cursos.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `nw201903`.`cursos` (
-  `course_cod` INT NOT NULL AUTO_INCREMENT,
-  `course_name` VARCHAR(45) NULL,
-  `course_desc` VARCHAR(100) NULL,
-  PRIMARY KEY (`course_cod`));
-
-INSERT INTO `nw201903`.`cursos` (`course_name`, `course_desc`) VALUES ('Combobox', 'Crear un combobox usando PHP');
-INSERT INTO `nw201903`.`cursos` (`course_cod`, `course_name`, `course_desc`) VALUES ('2', 'Tabla', 'Crear una tabla usando PHP y llenarla con datos de una Base de Datos MySQL');
